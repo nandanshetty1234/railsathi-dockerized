de27e36b911047002b35b6e6e1b5ad77cc8cec29
CodeIsNandan <nandan.badri818@gmail.com>
2025-07-11 19:47:05 +0530

    Add items app, docker-compose.yml, and requirements.txt

--- railsathi_django/settings.py
+++ railsathi_django/settings.py
@@ -72,10 +72,16 @@ WSGI_APPLICATION = 'railsathi_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT", 5432),
     }
 }
 
@@ -120,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+ALLOWED_HOSTS = ["*"]
